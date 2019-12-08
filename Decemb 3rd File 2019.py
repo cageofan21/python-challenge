dda528b3a20f67aa28608e9a64c9eafa9acaa1c1
Adib Khan <adib.cena@gmail.com>
2019-12-07 22:40:03 -0500

    Add Files via upload

--- Decemb 3rd File 2019.py
+++ Decemb 3rd File 2019.py
@@ -4,18 +4,20 @@ import csv
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-month_count = 0
+
 total_months = 0
 total_months_list = []
 total_net = 0 
 total_net_list = []
-average_changes = 0
 greatest_increase = 0
 greatest_decrease = 0
 max = 0
 min = 0
 min_month = " "
 max_month = " "
+net_average = 0
+net_change_one = 0 
+net_change_two = 0
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -32,11 +34,7 @@ with open(csvpath, newline="") as csvfile:
     #total Profit/Losses
     total_net = total_net + int(row[1])
     total_net_list.append(row[1])
-    print("Total:" + "$" + str(total_net))
-
-    #Average of changes in "Profit/Losses over entire period"
-    
-
+    print("Total: " + "$" + str(total_net))
 
     
     #Greatest Increase/Decrease in Profits/Losses over entire period
@@ -48,3 +46,10 @@ with open(csvpath, newline="") as csvfile:
         min_month = row[0] 
     print(f"Greatest Increase in Profits: {max_month} (${str(max)})")
     print(f"Greatest Decrease in Profits: {min_month} (${str(min)})")
+
+    #Average of changes in "Profit/Losses over entire period"
+    greatest_increase = net_change_one 
+    greatest_decrease = net_change_two
+
+    net_average = (net_change_one + net_change_two) / (2)  
+    print(f"Average Change: ")
